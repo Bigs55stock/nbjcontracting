88a09314ec552e3fba8a34ef684b6f0723e73aef
humza15 <humzazafarullah@gmail.com>
2023-03-06 11:52:21 -0500

    made urls in mainapp

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View 
+from django.http import HttpResponse 
 
-# Create your views here.
+
+
+class Home(View):
+
+    def get(self, request):
+        return HttpResponse("NBJ Home")
+
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("NBJ About")
